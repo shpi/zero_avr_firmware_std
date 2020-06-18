8391ded7ec43b3d29cac25294d2963e0a96cb0f4
SHPI <lh@shpi.de>
2020-06-18 14:58:08 +0100

    changed pwm clk to 62khz

--- main.c
+++ main.c
@@ -480,7 +480,7 @@ void setup(void)
    DDRC = 0b11000000;
    OCR0A = 0;           //    start value for FAN  0 / 255  (-> p-channel so inverted)       0x00 is ON  0xFF is OFF
    TCCR0B  =  0b00000001;
-   TCCR0A  =  0b10000011;            // 8bit dual slope 31khz
+   TCCR0A  =  0b10000011;            // 8bit  62khz
    TIMSK0 |= (1 << TOIE0);            // init interrupt for timer0 overflow
    clock_prescale_set(clock_div_1);
    I2C_init(I2C_ADDR);
@@ -521,7 +521,7 @@ int main(void)
   }                    
 
   wdt_reset();	
-  if (isrtimer > 31250)   // routine for calculate fan speed - timer is 32khz
+  if (isrtimer > 31250)   // routine for calculate fan speed - timer is 64khz
   {rpm = fanspin * 30;    // 2 signals each turn        
   fanspin = 0;
   isrtimer = 0;
